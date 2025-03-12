9c3fc704349e4f1a04447ebef21cd91a7aac273e
s27804 <s27804@pjwstk.edu.pl>
2025-03-13 00:09:24 +0100

    Dodano funkcje wyliczajaca srednia liczb calkowitych z tablicy

--- Aplikacja1/Program.cs
+++ Aplikacja1/Program.cs
@@ -1,5 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Modyfikacja 1");
 Console.WriteLine("Modyfikacja 2");
 Console.WriteLine("Modyfikacja 3");
+
+static double average(int[] table)
+{
+    int sum = 0;
+    for (int i = 0; i < table.Length; i++)
+        sum += table[i];
+
+    double result = sum / table.Length;
+    Console.WriteLine(result);
+    return result;
+}
+
+int[] table = [1,2,3,4,5];
+average(table);
